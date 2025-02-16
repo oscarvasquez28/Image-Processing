16637d5975e410ae2ba0fe081d2903339cc1e911
Óscar Fabián Vásquez Soto <oscar.vasquezot@uanl.edu.mx>
2025-02-15 18:00:57 -0600

    Cargar Video

--- Image-Processing/VideoController.Designer.cs
+++ Image-Processing/VideoController.Designer.cs
@@ -31,119 +31,129 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.VideoBox = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.CargarVideoBtn = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VideoBox)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(388, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(517, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(150, 236);
+            this.pictureBox1.Size = new System.Drawing.Size(200, 290);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(544, 0);
+            this.pictureBox2.Location = new System.Drawing.Point(725, 0);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(150, 115);
+            this.pictureBox2.Size = new System.Drawing.Size(200, 142);
             this.pictureBox2.TabIndex = 1;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(544, 121);
+            this.pictureBox3.Location = new System.Drawing.Point(725, 149);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(150, 115);
+            this.pictureBox3.Size = new System.Drawing.Size(200, 142);
             this.pictureBox3.TabIndex = 2;
             this.pictureBox3.TabStop = false;
             // 
-            // pictureBox4
+            // VideoBox
             // 
-            this.pictureBox4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.pictureBox4.BackgroundImage = global::Image_Processing.Properties.Resources.error_404;
-            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox4.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(351, 236);
-            this.pictureBox4.TabIndex = 3;
-            this.pictureBox4.TabStop = false;
+            this.VideoBox.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.VideoBox.BackgroundImage = global::Image_Processing.Properties.Resources.error_404;
+            this.VideoBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.VideoBox.Location = new System.Drawing.Point(0, 0);
+            this.VideoBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.VideoBox.Name = "VideoBox";
+            this.VideoBox.Size = new System.Drawing.Size(468, 290);
+            this.VideoBox.TabIndex = 3;
+            this.VideoBox.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(3, 264);
+            this.button1.Location = new System.Drawing.Point(4, 325);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 41);
+            this.button1.Size = new System.Drawing.Size(149, 50);
             this.button1.TabIndex = 4;
             this.button1.Text = "Pausa";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(121, 264);
+            this.button2.Location = new System.Drawing.Point(161, 325);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 41);
+            this.button2.Size = new System.Drawing.Size(149, 50);
             this.button2.TabIndex = 5;
             this.button2.Text = "Play";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(239, 264);
+            this.button3.Location = new System.Drawing.Point(319, 325);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(112, 41);
+            this.button3.Size = new System.Drawing.Size(149, 50);
             this.button3.TabIndex = 6;
             this.button3.Text = "Reiniciar Video";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(407, 264);
+            this.button4.Location = new System.Drawing.Point(543, 325);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(112, 41);
+            this.button4.Size = new System.Drawing.Size(149, 50);
             this.button4.TabIndex = 7;
             this.button4.Text = "Reiniciar Filtros";
             this.button4.UseVisualStyleBackColor = true;
             // 
-            // button5
+            // CargarVideoBtn
             // 
-            this.button5.Location = new System.Drawing.Point(562, 264);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(112, 41);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "Cargar Video";
-            this.button5.UseVisualStyleBackColor = true;
+            this.CargarVideoBtn.Location = new System.Drawing.Point(749, 325);
+            this.CargarVideoBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CargarVideoBtn.Name = "CargarVideoBtn";
+            this.CargarVideoBtn.Size = new System.Drawing.Size(149, 50);
+            this.CargarVideoBtn.TabIndex = 8;
+            this.CargarVideoBtn.Text = "Cargar Video";
+            this.CargarVideoBtn.UseVisualStyleBackColor = true;
+            this.CargarVideoBtn.Click += new System.EventHandler(this.CargarVideoBtn_Click);
             // 
             // VideoController
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.Controls.Add(this.button5);
+            this.Controls.Add(this.CargarVideoBtn);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.VideoBox);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "VideoController";
-            this.Size = new System.Drawing.Size(697, 325);
+            this.Size = new System.Drawing.Size(929, 414);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VideoBox)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -153,11 +163,11 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox VideoBox;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button CargarVideoBtn;
     }
 }
--- Image-Processing/VideoController.cs
+++ Image-Processing/VideoController.cs
@@ -1,20 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Image_Processing
 {
     public partial class VideoController : UserControl
     {
+        private VideoCapture _videoCapture;
+        private Timer _timer;
+        private Mat _frame;
+
         public VideoController()
         {
             InitializeComponent();
+            _frame = new Mat();
+            _timer = new Timer();
+            _timer.Interval = 33; // Aproximadamente 30 FPS
+            _timer.Tick += Timer_Tick;
+        }
+
+        private void CargarVideoBtn_Click(object sender, EventArgs e)
+        {
+            // Abre el archivo de video
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Archivos de video|*.mp4;*.avi;*.mov;*.mkv";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    _videoCapture = new VideoCapture(openFileDialog.FileName);
+                    _timer.Start(); // Comienza a reproducir el video
+                }
+            }
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            // Lee el siguiente fotograma
+            if (_videoCapture.Read(_frame) && !_frame.IsEmpty)
+            {
+                // Redimensiona la imagen para que se ajuste al tamaño del PictureBox
+                Bitmap resizedImage = new Bitmap(_frame.ToBitmap(), VideoBox.Size);
+
+                // Libera la imagen anterior para evitar fugas de memoria
+                VideoBox.Image?.Dispose();
+
+                // Asigna la nueva imagen redimensionada
+                VideoBox.Image = resizedImage;
+            }
+            else
+            {
+                _timer.Stop(); // Detiene el temporizador cuando el video se haya terminado
+            }
+        }
+
+
+        // Método para liberar manualmente los recursos
+        public void ReleaseResources()
+        {
+            _timer?.Stop(); // Detiene el temporizador
+            _videoCapture?.Dispose(); // Libera el objeto VideoCapture
+            _frame?.Dispose(); // Libera el objeto Mat
         }
     }
 }
